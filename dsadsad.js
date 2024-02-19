a05471e6a170c7e442ce5f3325d8ba8691f98381
dFriggi <davifriggi2005@gmail.com>
2024-02-19 13:53:39 -0300

    meus 2 codigos para o desafio do nivel do heroi, um utilizando condicionais (if, else if e else), e o outro utilizando o switch case

---
+++ dsadsad.js
@@ -0,0 +1,12 @@
+let x = 5; // Your expression or value
+
+switch (true) {
+  case x < 3:
+    console.log("x is less than 3");
+    break;
+  case x < 6:
+    console.log("x is less than 6");
+    break;
+  default:
+    console.log("x is greater than or equal to 6");
+}
