61a972b713d92e526e6e70d00ffa81b7b0da687c
dFriggi <davifriggi2005@gmail.com>
2024-02-19 16:11:56 -0300

    editei os codigos para que eles tenham uma variavel especifica para o rank do heroi, para seguir exatamente o que o desafio propunha

--- index (condicionais).js
+++ index (condicionais).js
@@ -1,20 +1,23 @@
 let nomeHeroi = "Espartacus"
-let xpHeroi = 10500
+let xpHeroi = 100
+let rankHeroi
 
 if (xpHeroi <= 1000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+    rankHeroi = "Ferro"
 }else if(xpHeroi >= 1001 && xpHeroi<= 2000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+    rankHeroi = "Bronze"
 }else if(xpHeroi >= 2001 && xpHeroi<= 5000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+    rankHeroi = "Prata"
 }else if(xpHeroi >= 5001 && xpHeroi<= 7000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+    rankHeroi = "Ouro"
 }else if(xpHeroi >= 7001 && xpHeroi<= 8000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+    rankHeroi = "Platina"
 }else if(xpHeroi >= 8001 && xpHeroi<= 9000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+    rankHeroi = "Ascendente"
 }else if(xpHeroi >= 9001 && xpHeroi<= 10000){
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+    rankHeroi = "Imortal"
 }else {
-    console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+    rankHeroi = "Radiante"
 }
+
+console.log("O Herói de nome " + nomeHeroi + " está no nível de " + rankHeroi)
--- index (switch).js
+++ index (switch).js
@@ -1,35 +1,38 @@
 let nomeHeroi = "Espartacus"
 let xp = 10001
+let rankHeroi
 
 switch(true){
     case xp <= 1000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ferro")
+        rankHeroi = "Ferro"
         break
     
     case xp >= 1001 && xp <= 2000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Bronze")
+        rankHeroi = "Bronze"
         break
     
     case xp >= 2001 && xp <= 5000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Prata")
+        rankHeroi = "Prata"
         break
 
     case xp >= 5001 && xp <= 7000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ouro")
+        rankHeroi = "Ouro"
         break
     
     case xp >= 7001 && xp <= 8000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Platina")
+        rankHeroi = "Platina"
         break
 
     case xp >= 8001 && xp <= 9000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Ascendente")
+        rankHeroi = "Ascendente"
         break
     
     case xp >= 9001 && xp <= 10000:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Imortal")
+        rankHeroi = "Imortal"
         break
     
-    default:
-        console.log("O Herói de nome " + nomeHeroi + " está no nível de Radiante")
+    case xp >= 10001:
+        rankHeroi = "Radiante"
+        break
 }
+console.log("O herói de nome " + nomeHeroi + " está no nível de " + rankHeroi)
